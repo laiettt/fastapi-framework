de4e4955c4e2a307550b19574353653d5285b02b
master <tim60911@gmail.com>
2024-03-24 21:22:14 +0800

    change api path and return message

--- routers/member.py
+++ routers/member.py
@@ -3,14 +3,14 @@ from model.member.login import LoginRequest
 from common.enum import Gender
 
 router = APIRouter(
-    prefix="/qa/api",
+    prefix="/qa/api/member",
     tags=["Member"],
 )
 
 
-@router.post(path="/member")
+@router.post(path="/login")
 async def login(request: LoginRequest) -> dict:
     if request.gender:
         if request.gender != Gender.Male.value and request.gender != Gender.Female.value:
             raise HTTPException(status_code=422, detail="Unprocessable Entity")
-    return {"code": "0000", "message": "success"}
+    return {"code": "0000", "message": "成功"}
